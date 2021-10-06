2344476a64ceecbfba872bd57ace8a654a5a3cc1
Lika Vorobyeva <lika@solarlabs.ee>
2021-10-06 12:03:59 +0700

    Add node info to session

--- Sources/SentinelWallet/Models/Sentinel/Session.swift
+++ Sources/SentinelWallet/Models/Sentinel/Session.swift
@@ -10,9 +10,11 @@ import Foundation
 public struct Session {
     public let id: UInt64
     public let durationInSeconds: Int64
+    public let node: String
 
     init(from response: Sentinel_Session_V1_Session) {
         id = response.id
         durationInSeconds = response.duration.seconds
+        node = response.node
     }
 }
