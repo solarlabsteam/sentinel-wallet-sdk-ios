e692e5403dc864b43c52587ae1870e5390c62c22
Lika Vorobeva <lika@solarlabs.ee>
2022-01-18 15:33:51 +0200

    Clear up

--- iOS Example/Sources/ContentView.swift
+++ iOS Example/Sources/ContentView.swift
@@ -9,19 +9,6 @@ import SwiftUI
 import SentinelWallet
 
 struct ContentView: View {
-    private let wallet = WalletManager(securityService: SecurityService())
-        .wallet(for: "sent1e7fka52pfqdushfezeg3w6swduklfskzx6vmfu")
-    init() {
-        Config.setup()
-        let mnemonicsToAdd = [
-            "inherit", "child", "tumble", "brick", "lizard", "old", "style", "captain", "hand",
-            "echo", "rookie", "flower", "topic", "curve", "kangaroo", "mail", "shaft", "improve",
-            "fun", "beach", "recycle", "scrap", "icon", "involve"
-        ]
-
-        log.debug(wallet.add(mnemonics: mnemonicsToAdd))
-    }
-
     var body: some View {
         NavigationView {
             List {
