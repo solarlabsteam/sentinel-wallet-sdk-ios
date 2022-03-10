4402499bfa389bb336c60945b6f9c524a1d068e1
VictoriaKostyleva <kostyleva.viktoriya@gmail.com>
2022-03-10 12:12:47 +0200

    Add rawLog to TransactionResult

--- Sources/SentinelWallet/Models/Sentinel/TransactionResult.swift
+++ Sources/SentinelWallet/Models/Sentinel/TransactionResult.swift
@@ -11,10 +11,12 @@ public struct TransactionResult {
     public let isSuccess: Bool
     public let timestamp: String
     public let txHash: String
+    public let rawLog: String
 
     init(from response: Cosmos_Base_Abci_V1beta1_TxResponse) {
         isSuccess = response.code == 0
         timestamp = response.timestamp
         txHash = response.txhash
+        rawLog = response.rawLog
     }
 }
