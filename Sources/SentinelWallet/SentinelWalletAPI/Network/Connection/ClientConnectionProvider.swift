797ec168241824f723686759a30e8e1801d0aabe
Lika Vorobeva <lika@solarlabs.ee>
2022-05-23 14:55:34 +0300

    Update default host for requests

--- Sources/SentinelWallet/SentinelWalletAPI/Network/Connection/ClientConnectionProvider.swift
+++ Sources/SentinelWallet/SentinelWalletAPI/Network/Connection/ClientConnectionProvider.swift
@@ -10,7 +10,8 @@ import GRPC
 import NIO
 
 private struct Constants {
-    let hostString = "lcd-sentinel-app.cosmostation.io"
+    let hostString = "lcd-sentinel.dvpn.solar"
+    let port = 993
 }
 private let constants = Constants()
 
@@ -19,12 +20,21 @@ protocol ClientConnectionProviderType {
 }
 
 final class ClientConnectionProvider: ClientConnectionProviderType {
+    private let hostString: String
+    private let port: Int
+    
+    init(host: String = constants.hostString, port: Int = constants.port) {
+        self.hostString = host
+        self.port = port
+    }
+    
     func openConnection(for work: @escaping (ClientConnection) -> Void) {
-        DispatchQueue.global().async {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
             let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
             defer { try! group.syncShutdownGracefully() }
             
-            let channel = ClientConnection.insecure(group: group).connect(host: constants.hostString, port: 9090)
+            let channel = ClientConnection.insecure(group: group).connect(host: self.hostString, port: self.port)
             defer { try! channel.close().wait() }
             
             work(channel)
