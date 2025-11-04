f160232f849250063eb53946772110c9a0904a60
Lika <lika@solarlabs.ee>
2025-11-04 17:22:19 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SentinelWallet",
     platforms: [
-        .iOS(.v13), .macOS(.v11)
+        .iOS(.v15), .macOS(.v12)
     ],
     products: [
         .library(
@@ -15,17 +15,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.19.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMinor(from: "2.0.0")),
-        .package(url: "https://github.com/solarlabsteam/HDWallet", .branch("spm"))
+        .package(url: "https://github.com/lika-vorobeva/HDWallet", .branch("swift-6"))
     ],
     targets: [
         .target(
             name: "SentinelWallet",
             dependencies: [
-                "SwiftProtobuf", "HDWallet", "SwiftyBeaver",
+                "HDWallet", "SwiftyBeaver",
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
